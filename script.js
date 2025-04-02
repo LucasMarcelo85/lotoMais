2adf19e7c9ef6144102454158e76d9515a207110
Marcelo <devfullmarcelo@gmail.com>
2025-04-02 18:25:17 -0300

    fim

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-// setTimeout(function() {
-//     window.location.href = "https://wa.me/554791144904?text=Vim%20do%20site%20e%20gostaria%20de%20informa%C3%A7%C3%B5es!";
-// }, 3000); // 3000 milissegundos = 3 segundos
+setTimeout(function() {
+    window.location.href = "https://wa.me/554791144904?text=Vim%20do%20site%20e%20gostaria%20de%20informa%C3%A7%C3%B5es!";
+}, 3000); // 3000 milissegundos = 3 segundos
